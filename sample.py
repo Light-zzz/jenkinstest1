f2326ca2b09f8500601e7facace186ec8f5888c4
Dipesh <dipeshvishwa767@gmail.com>
2025-10-05 17:00:30 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi, frnd whats up'
+    return 'Hello Frnd Whats up?'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
